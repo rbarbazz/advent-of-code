5f7f46c62318582e83d968ec9976a967fa16583e
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2021-12-05 22:50:49 -0800

    feat: d04 part two

---
+++ 2021/04/part-two.js
@@ -0,0 +1,56 @@
+const { draw } = require('./input')
+let { boards } = require('./input')
+
+let winnerBoard
+let lastNum
+
+const isWinner = (board) => {
+  if (board.find((line) => line.every((num) => num === null))) {
+    return true
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    if (
+      board[0][i] === null &&
+      board[1][i] === null &&
+      board[2][i] === null &&
+      board[3][i] === null &&
+      board[4][i] === null
+    )
+      return true
+  }
+
+  return false
+}
+
+for (let i = 0; i < draw.length; i++) {
+  lastNum = draw[i]
+
+  boards.forEach((board) => {
+    const [x, y] = board.reduce(
+      (res, line, index) => {
+        const lineMatchIndex = line.indexOf(lastNum)
+
+        if (lineMatchIndex > -1) return [lineMatchIndex, index]
+
+        return res
+      },
+      [null, null],
+    )
+
+    if (x !== null && y !== null) board[y][x] = null
+  })
+
+  winnerBoard = boards.find(isWinner)
+  boards = boards.filter((board) => !isWinner(board))
+
+  if (boards.length === 0) {
+    break
+  }
+}
+
+const sumNums = winnerBoard
+  .flat(2)
+  .reduce((acc, curr) => (curr !== null ? acc + curr : acc), 0)
+
+console.log(sumNums * lastNum)
